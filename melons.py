7645b6121a46f09908bddf654c1ff16f068b23c3
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-13 13:57:36 -0700

    started Government Melon Order

--- melons.py
+++ melons.py
@@ -17,6 +17,10 @@ class AbstactOrder(object):
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species == "Christmas":
+            base_price *= 1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -37,7 +41,7 @@ class DomesticMelonOrder(AbstactOrder):
     order_type = "domestic"
     tax = 0.08
 
-    def __init__(self, species, qty, country_code):
+    def __init__(self, species, qty):
         """Initialize melon order attributes."""
 
         super(InternationalMelonOrder, self).__init__(species, qty)
@@ -60,3 +64,30 @@ class InternationalMelonOrder(AbstactOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+        """Total for int'l orders including flat shipping fee."""
+
+        result = super(InternationalMelonOrder,self).get_total()
+
+        if self.qty < 10:
+            result += 3
+
+        return result
+
+
+class GovernmentMelonOrder(AbstactOrder):
+    """A melon order from the Government OMG."""
+
+    order_type = "government"
+    tax = 0.00
+    passed_inspection = False
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+
+        super(GovernmentMelonOrder, self).__init__(species, qty)
+
+
+    def mark_inspection(self, is_passed):
+        passed_inspection = is_passed
